71fe3ba61775ff6de5b4ac2c3b54b4fc9bf534d3
Latorre Ahron A. <latorre061904@gmail.com>
2025-02-16 21:18:28 +0800

    Add files via upload

---
+++ PRELIM_HO2_BSIT32A3_LATORRE_AHRON/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('hobbies-btn').addEventListener('click', function() {
+    alert('My hobbies include reading, listening to music, hugging my pet, coding, and drawing!');
+});
+
+document.getElementById('Send-btn').addEventListener('click', function() {
+    alert('Your Message has been Sent!');
+});
